da93eed22a5ac8658e56a8b804401ddedb65e417
Gedoix <diegoortegop@hotmail.com>
2019-04-08 15:37:04 -0400

    deleted useless javascript

--- prototype/js/sidebar.js
+++ prototype/js/sidebar.js
@@ -7,27 +7,5 @@ $("#menu-toggle").click(
     {
         e.preventDefault();
         $("#wrapper").toggleClass("toggled");
-
-        // if ($("#sign-out-button").hasClass("col-md-3") 
-        //         && $("#sign-out-button").hasClass("col-lg-2") 
-        //         && $("#sign-out-button").hasClass("col-xl-1"))
-        // {
-        //     $("#sign-out-button").removeClass("col-md-3");
-        //     $("#sign-out-button").addClass("col-md-4");
-        //     $("#sign-out-button").removeClass("col-lg-2");
-        //     $("#sign-out-button").addClass("col-lg-3");
-        //     $("#sign-out-button").removeClass("col-xl-1");
-        //     $("#sign-out-button").addClass("col-xl-2");
-        // }
-        // else
-        // {
-        //     $("#sign-out-button").removeClass("col-md-4");
-        //     $("#sign-out-button").addClass("col-md-3");
-        //     $("#sign-out-button").removeClass("col-lg-3");
-        //     $("#sign-out-button").addClass("col-lg-2");
-        //     $("#sign-out-button").removeClass("col-xl-2");
-        //     $("#sign-out-button").addClass("col-xl-1");
-        // }
-        
     }
 );
